eee4dda73f790dad3e5e91547d743e38e6605b10
Fagner Silva <fagnermarketing2014@gmail.com>
2022-09-27 10:25:15 -0300

    Initial

---
+++ src/components/Posts/index.js
@@ -0,0 +1,10 @@
+import './style.css';
+import { PostCard } from "../PostCard";
+
+export const Posts = ({ posts }) => (
+    <div className="posts">
+        {posts.map(post => (
+            <PostCard key={post.id} post={post} />
+        ))}
+    </div>
+);
